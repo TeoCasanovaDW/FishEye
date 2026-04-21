5ed593aed6f3b1c62358b60d84aa176572d8f3c0
Teo <teo.casanova13@gmail.com>
2026-04-21 11:41:15 +0200

    home page integration with photographers list

--- app/page.js
+++ app/page.js
@@ -1,18 +1,61 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { getAllPhotographers } from "./lib/prisma-db";
-import { use } from "react";
+import Link from "next/link";
 
 
-export default function Home() {
-  // console.log(await getAllPhotographers())
-  // console.log(await getPhotographer(0))
-  // console.log(await getAllMediasForPhotographer())
-
-  const photographers = use(getAllPhotographers())
-  console.log(photographers);
+export default async function Home() {
+  
+  const photographers = await getAllPhotographers()
 
   return (
-    <div></div>
+    <>
+      <header className={styles.banner}>
+        <Link
+          href="/"
+          className={styles.logoLink}
+        >
+          <Image
+            src="/assets/logo.png"
+            alt="Fisheye Home page"
+            width={200}
+            height={50}
+            className={styles.logo}
+          />
+        </Link>
+        <h1>Nos photographes</h1>
+      </header>
+
+      <main>
+        <section className={styles.cardsContainer}>
+          {photographers.map(photographer => (
+            <article 
+              key={photographer.id}
+              className={styles.photographerCard}
+            >
+              <Link
+                href={`/photographer/${photographer.id}`}
+                className={styles.photographerLink}
+              >
+                <Image
+                  src={`/assets/${photographer.portrait}`}
+                  alt={`Portrait de ${photographer.name}`}
+                  width={526}
+                  height={358}
+                  className={styles.portrait}
+                />
+                <h2 className={styles.photographerName}>{photographer.name}</h2>
+              </Link>
+
+              <div className={styles.photographerInfos}>
+                <p className={styles.localisation}>{photographer.city}, {photographer.country}</p>
+                <p className={styles.sentence}>{photographer.tagline}</p>
+                <p className={styles.price}>{photographer.price}€/jour</p>
+              </div>
+            </article>
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
